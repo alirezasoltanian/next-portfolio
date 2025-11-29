b59cf7e1f688f3e78641317ba76f55999ef38f89
a.soltanian <ali76rez@gmail.com>
2025-11-29 22:04:51 +0330

    refactor: enhance ButtonGooey component with improved type definitions and consistent styling

--- src/components/ButtonGooey.tsx
+++ src/components/ButtonGooey.tsx
@@ -2,7 +2,7 @@
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import { useMounted } from "../hooks/use-mount";
 export const ButtonGooey = () => {
   // const variants = {
@@ -10,63 +10,69 @@ export const ButtonGooey = () => {
   //   closed: { opacity: 0, x: "-100%" , rotate: '00deg' },
   // };
   const mounted = useMounted();
-  const variants = {
+  const variants: Variants = {
     open: { rotate: "360deg" },
-    closed: { rotate: "0deg", transition: { ease: "easeOut", duration: 0.5 } },
+    closed: {
+      rotate: "0deg",
+      transition: {
+        ease: [0.17, 0.67, 0.83, 0.67],
+        duration: 0.5,
+      },
+    },
   };
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
   return (
     <>
-      <div className='wrapper relative'>
-        <p className='absolute -top-2 left-0 right-0 m-auto text-[8px] text-black'>
+      <div className="wrapper relative">
+        <p className="absolute -top-2 left-0 right-0 m-auto text-[8px] text-black">
           Skill issue
         </p>
         {mounted ? (
           <button
             onClick={() => setIsOpen((isOpen) => !isOpen)}
-            className='button'
+            className="button"
           >
             ( •_•)&gt;⌐■-■
-            <div className=' bubble'>
+            <div className=" bubble">
               <motion.div
                 animate={isOpen ? "open" : "closed"}
                 variants={variants}
               >
                 <span>(</span>
-                <span className=' text-black'>⌐■</span>
+                <span className=" text-black">⌐■</span>
                 <span>_</span>
-                <span className=' text-black'>■</span>
+                <span className=" text-black">■</span>
                 <span>)</span>
               </motion.div>
             </div>
           </button>
         ) : (
-          <button className='w-32 h-16  rounded-xl mt-3'></button>
+          <button className="w-32 h-16  rounded-xl mt-3"></button>
         )}
       </div>
 
       <svg
-        className='absolute hidden'
-        width='0'
-        height='0'
-        xmlns='http://www.w3.org/2000/svg'
-        version='1.1'
+        className="absolute hidden"
+        width="0"
+        height="0"
+        xmlns="http://www.w3.org/2000/svg"
+        version="1.1"
       >
         <defs>
-          <filter id='gooey'>
+          <filter id="gooey">
             <feGaussianBlur
-              in='SourceGraphic'
-              stdDeviation='10'
-              result='blur-sm'
+              in="SourceGraphic"
+              stdDeviation="10"
+              result="blur-sm"
             />
             <feColorMatrix
-              in='blur-sm'
-              mode='matrix'
-              values='1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10'
-              result='gooey'
+              in="blur-sm"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 20 -10"
+              result="gooey"
             />
-            <feComposite in='SourceGraphic' in2='gooey' operator='atop' />
+            <feComposite in="SourceGraphic" in2="gooey" operator="atop" />
           </filter>
         </defs>
       </svg>
