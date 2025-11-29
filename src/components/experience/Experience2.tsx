4720014f0c805a4077b9893ef0ed09180db9b219
a.soltanian <ali76rez@gmail.com>
2025-11-29 22:39:19 +0330

    feat: add new job entries for Solop and Dideban in Experience2 component and update headings to Persian

--- src/components/experience/Experience2.tsx
+++ src/components/experience/Experience2.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ExperienceCard from "./ExperienceCard";
 const experienceData = [
   {
@@ -14,7 +13,7 @@ const experienceData = [
   },
   {
     company: "Kasbino",
-    link: "https://parabol.co",
+    link: "https://kasbinoapp.com/",
     badges: ["React", "bootstrap"],
     title: "Front-end Developer",
     start: "8 Months",
@@ -22,11 +21,56 @@ const experienceData = [
     description:
       "I worked for 8 months on a management dashboard and developed a tool for creating UI using Drag & Drop, which served as a UI Builder for management dashboards. The main challenge in this project was controlling and managing the Drag & Drop elements without using pre-built packages.",
   },
+  {
+    company: "Solop",
+    link: "https://solop.ir/",
+    badges: [
+      "Next.js",
+      "Drizzle",
+      "ai-sdk",
+      "OpenAI",
+      "Zustand",
+      "Ethers",
+      "NextAuth",
+      "Shadcn",
+      "Payment",
+      "Socket.io",
+      "Postgres",
+      "Tailwind",
+      "Nodemailer",
+    ],
+    title: "Full Stack Developer",
+    start: "2024",
+    end: "Present",
+    description:
+      "Solop is a platform for creating online stores with features to manage orders, products, users, and payment gateways. The system includes an AI assistant and a store agent to make the buying and selling experience smarter and easier for both customers and vendors.",
+  },
+  {
+    company: "Dideban",
+    link: "https://dideban.solop.ir/",
+    badges: [
+      "Next.js",
+      "Drizzle",
+      "ai-sdk",
+      "OpenAI",
+      "Zustand",
+      "NextAuth",
+      "Shadcn",
+      "Payment",
+      "Postgres",
+      "Tailwind",
+    ],
+    title: "Full Stack Developer",
+    start: "2025",
+    end: "Present",
+    description:
+      "Dideban is a tool for the Divar platform that intelligently notifies users about new listings and important updates such as price or description changes. Notifications are delivered via chat and push alerts, providing a fast experience without the need for constant searching.",
+  },
 ];
 function Experience2() {
   return (
     <div className="flex mt-2 flex-col px-12 justify-center items-center">
-      <h2 className="text-brand-foreground">My experience</h2>
+      <h2 className="text-brand-foreground">سوابق کاری</h2>
       <div className="flex gap-5 flex-wrap mt-2">
         {experienceData.map((item, index) => (
           <div className="" key={index}>
